5380c4100e381dc5d6af2c9af7c9e7d18bca2dbc
Enrique <herreraenrike2@gmail.com>
2017-12-05 11:21:51 -0600

    Clase Nodo

--- src/sample/Nodo.java
+++ src/sample/Nodo.java
@@ -1,7 +1,36 @@
 package sample;
 
 /**
- * Created by Rosbin on 04/12/2017.
+ * Created by George on 03/12/2017.
  */
 public class Nodo {
+    private int dato;
+    private Nodo izq; // punter hacia el siguiente nodo
+    private Nodo der;
+
+    //constructor, inicializa el nodo
+    public Nodo(int dato){
+        this.dato =dato;
+    }
+    //Los getters
+    public Nodo getNodoIzquierdo(){
+        return izq;
+    }
+
+    public Nodo getNodoDerecho(){
+        return der;
+    }
+    // setters
+    public void setNodoIzquierdo(Nodo nodo){
+        izq = nodo;
+    }
+
+    public void setNodoDerecho(Nodo nodo){
+        der = nodo;
+    }
+
+    public int getDato(){
+        return dato;
+    }
+
 }
