40ef055cc522e71b10e57d173533453a4c89bb0b
Rosbin <lezamarosbin98@gmail.com>
2017-12-05 11:32:53 -0600

    correcion de errrores

--- src/sample/MetodosArbol.java
+++ src/sample/MetodosArbol.java
@@ -43,50 +43,5 @@ public class MetodosArbol {
             return mensaje;
         }
 
-        public String tamaño (Nodo raiz){
-            if(raiz != null){
-                tamaño++;
-                tamaño(raiz.getNodoIzquierdo());
-                tamaño(raiz.getNodoDerecho());
-            }
-            return "El tamaño de nodos es : "+ tamaño;
-        }
-        public String mayor(Nodo raiz){
-            if(raiz != null){
-                if (mayor < raiz.getDato()){
-                    mayor = raiz.getDato();
-                }
-                mayor(raiz.getNodoIzquierdo());
-                mayor(raiz.getNodoDerecho());
-            }
-            return "El valor MAYOR es: "+ mayor;
-        }
-
-        public String menor(Nodo raiz){
-            if(raiz != null){
-                if (menor > raiz.getDato()){
-                    menor = raiz.getDato();
-                }
-                menor(raiz.getNodoIzquierdo());
-                menor(raiz.getNodoDerecho());
-            }
-            return "El valor MENOR es: "+ menor;
-        }
-
-
-        public String buscar(Nodo raiz, int  r){
-            if(raiz != null){
-                if (raiz.getDato()==r){
-                    encontrado = true;
-                    mensaje = r + " Encontrado en el arbol";
-                }
-                buscar(raiz.getNodoIzquierdo(),r);
-                buscar(raiz.getNodoDerecho(),r);
-            }
-            if (encontrado == false){
-                mensaje = r + " NO encontrado";
-            }
-            return mensaje;
-        }
-
+        
     }
