d871ffa609e432f37d0cf3bca93c8059bd0413d9
Rosbin <lezamarosbin98@gmail.com>
2017-12-05 11:12:25 -0600

    modifique metodos

--- src/sample/MetodosArbol.java
+++ src/sample/MetodosArbol.java
@@ -4,4 +4,14 @@ package sample;
  * Created by Rosbin on 04/12/2017.
  */
 public class MetodosArbol {
+    public class MetodosArbol {
+        private int tamaño=0;
+        int mayor=0;
+        int menor = 9999;
+        boolean encontrado = false;
+        String mensaje = "";
+
+        public void mensaje (){
+            mensaje="";
+        }
 }
