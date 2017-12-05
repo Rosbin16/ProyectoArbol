a1cbe4a0dd524cd035f5b5d1008bae2c4ec975b2
George <jorgealva725@gmail.com>
2017-12-05 11:26:31 -0600

    Métodos mayor y menor

--- src/sample/MetodosArbol.java
+++ src/sample/MetodosArbol.java
@@ -4,4 +4,29 @@ package sample;
  * Created by Rosbin on 04/12/2017.
  */
 public class MetodosArbol {
+    int mayor=0;
+    int menor = 9999;
+
+    public String mayor(Nodo raiz){
+        if(raiz != null){
+            if (mayor < raiz.getDato()){
+                mayor = raiz.getDato();
+            }
+            mayor(raiz.getNodoIzquierdo());
+            mayor(raiz.getNodoDerecho());
+        }
+        return "El valor MAYOR es: "+ mayor;
+    }
+
+    public String menor(Nodo raiz){
+        if(raiz != null){
+            if (menor > raiz.getDato()){
+                menor = raiz.getDato();
+            }
+            menor(raiz.getNodoIzquierdo());
+            menor(raiz.getNodoDerecho());
+        }
+        return "El valor MENOR es: "+ menor;
+    }
+
 }
